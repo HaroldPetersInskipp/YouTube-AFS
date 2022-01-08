69174ed963c3c15b4e83866af11a3ecdea1efb6b
Inskipp <71417979+HaroldPetersInskipp@users.noreply.github.com>
2022-01-07 21:49:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Youtube - Auto Focus Search Bar
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/HaroldPetersInskipp
 // @version      1.0
+// @homepage     https://github.com/HaroldPetersInskipp/YouTube-AFS
 // @description  Auto focus the search bar in YouTube.
 // @author       Inskipp
 // @icon         https://raw.githubusercontent.com/HaroldPetersInskipp/YouTube-AFS/main/icon.png
+// @match        *://www.youtube.com/*
 // ==/UserScript==
 var search_panel = document.getElementById("search-input").children[0].focus();
