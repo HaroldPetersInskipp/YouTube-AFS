4edeec4ca2e426d98bf9b53995b39e59dfc572d2
Inskipp <71417979+HaroldPetersInskipp@users.noreply.github.com>
2022-07-31 16:48:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 // @name         Youtube - Auto Focus Search Bar
 // @namespace    https://github.com/HaroldPetersInskipp
 // @version      1.0
-// @homepage     https://github.com/HaroldPetersInskipp/YouTube-AFS
+// @homepageURL  https://github.com/HaroldPetersInskipp/YouTube-AFS
+// @supportURL   https://github.com/HaroldPetersInskipp/YouTube-AFS/issues
 // @description  Auto focus the search bar in YouTube.
 // @author       Inskipp
-// @icon         https://raw.githubusercontent.com/HaroldPetersInskipp/YouTube-AFS/main/icon.png
+// @copyright    2022+, HaroldPetersInskipp (https://github.com/HaroldPetersInskipp)
+// @license      MIT; https://github.com/HaroldPetersInskipp/YouTube-AFS/master/LICENSE
 // @match        *://www.youtube.com/*
+// @icon         https://raw.githubusercontent.com/HaroldPetersInskipp/YouTube-AFS/main/icon.png
+// @grant        none
 // ==/UserScript==
+
 var search_panel = document.getElementById("search-input").children[0].focus();
