2256c574b3b33e1e2036fe960bbe3a00e5c63537
Inskipp <71417979+HaroldPetersInskipp@users.noreply.github.com>
2025-09-01 08:12:46 -0600

    Update script.js
    Removed comment about updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube - Auto Focus Search Bar
 // @namespace    https://github.com/HaroldPetersInskipp
-// @version      1.4.4
+// @version      1.4.5
 // @homepageURL  https://github.com/HaroldPetersInskipp/YouTube-AFS
 // @supportURL   https://github.com/HaroldPetersInskipp/YouTube-AFS/issues
 // @downloadURL  https://gist.githubusercontent.com/HaroldPetersInskipp/5129292f3750d99735c2a8b352a5651c/raw/youtube-afs.user.js
@@ -34,4 +34,3 @@
     const observer = new MutationObserver(focusSearch);
     observer.observe(document.body, { childList: true, subtree: true });
 })();
-// Just testing updating feature again (last time pls)
