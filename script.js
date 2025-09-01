8e9b64fe3c6278f2fddbfae6bf2be4e084a79c91
Inskipp <71417979+HaroldPetersInskipp@users.noreply.github.com>
2025-09-01 08:11:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Youtube - Auto Focus Search Bar
 // @namespace    https://github.com/HaroldPetersInskipp
-// @version      1.4.3
+// @version      1.4.4
 // @homepageURL  https://github.com/HaroldPetersInskipp/YouTube-AFS
 // @supportURL   https://github.com/HaroldPetersInskipp/YouTube-AFS/issues
-// @downloadURL  https://gist.githubusercontent.com/HaroldPetersInskipp/5129292f3750d99735c2a8b352a5651c/raw/
-// @updateURL    https://gist.githubusercontent.com/HaroldPetersInskipp/5129292f3750d99735c2a8b352a5651c/raw/
+// @downloadURL  https://gist.githubusercontent.com/HaroldPetersInskipp/5129292f3750d99735c2a8b352a5651c/raw/youtube-afs.user.js
+// @updateURL    https://gist.githubusercontent.com/HaroldPetersInskipp/5129292f3750d99735c2a8b352a5651c/raw/youtube-afs.user.js
 // @description  Auto focus the search bar in YouTube.
 // @author       Inskipp
 // @copyright    2024+, HaroldPetersInskipp (https://github.com/HaroldPetersInskipp)
@@ -34,4 +34,4 @@
     const observer = new MutationObserver(focusSearch);
     observer.observe(document.body, { childList: true, subtree: true });
 })();
-// Just testing updating feature again
+// Just testing updating feature again (last time pls)
